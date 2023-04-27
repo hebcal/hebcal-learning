c61e7982b7fb1569de074dd9178ece008caf8080
Michael J. Radwin <michael@radwin.org>
2023-04-26 21:15:58 -0700

    Working

---
+++ rollup.config.js
@@ -0,0 +1,97 @@
+const babel = require('@rollup/plugin-babel');
+const json = require('@rollup/plugin-json');
+const terser = require('@rollup/plugin-terser');
+const pkg = require('./package.json');
+
+const banner = '/*! ' + pkg.name + ' v' + pkg.version + ' */';
+
+module.exports = [
+  {
+    input: 'src/index.js',
+    output: [
+      {file: pkg.main, format: 'cjs', name: pkg.name, banner},
+    ],
+    plugins: [
+      json({compact: true}),
+      babel({
+        babelHelpers: 'bundled',
+        presets: [
+          ['@babel/preset-env', {
+            modules: false,
+            targets: {
+              node: '12.22.0',
+            },
+          }],
+        ],
+        exclude: ['node_modules/**'],
+      }),
+    ],
+    external: ['@hebcal/core'],
+  },
+  {
+    input: 'src/index.js',
+    output: [
+      {file: pkg.module, format: 'es', name: pkg.name, banner},
+    ],
+    plugins: [
+      json({compact: true, preferConst: true}),
+      babel({
+        babelHelpers: 'bundled',
+        presets: [
+          ['@babel/preset-env', {
+            modules: false,
+            targets: {
+              node: '12.22.0',
+            },
+          }],
+        ],
+        exclude: ['node_modules/**'],
+      }),
+    ],
+    external: ['@hebcal/core'],
+  },
+  {
+    input: 'src/index.js',
+    output: [
+      {
+        file: 'dist/bundle.js',
+        format: 'iife',
+        name: 'hebcal__learning',
+        globals: {
+          '@hebcal/core': 'hebcal',
+        },
+        indent: false,
+        banner,
+      },
+      {
+        file: 'dist/bundle.min.js',
+        format: 'iife',
+        name: 'hebcal__rest_api',
+        globals: {
+          '@hebcal/core': 'hebcal',
+        },
+        plugins: [terser()],
+        banner,
+      },
+    ],
+    plugins: [
+      json({compact: true}),
+      babel({
+        babelHelpers: 'bundled',
+        presets: [
+          ['@babel/preset-env', {
+            modules: false,
+            targets: {
+              chrome: '103',
+              safari: '13.1',
+            },
+            useBuiltIns: 'usage',
+            corejs: 3,
+          }],
+        ],
+        exclude: ['node_modules/**'],
+      }),
+    ],
+    external: ['@hebcal/core'],
+  },
+];
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import {DafYomiEvent} from './dafyomi';
+import {MishnaYomiEvent} from './MishnaYomiEvent';
+import {MishnaYomiIndex, mishnaYomiStart} from './mishnaYomi';
+import {NachYomiEvent} from './NachYomiEvent';
+import {NachYomiIndex, nachYomiStart} from './nachYomi';
+import {yerushalmiYomi, YerushalmiYomiEvent, vilna, schottenstein} from './yerushalmi';
+
+
+export {DafYomiEvent, MishnaYomiEvent,
+  MishnaYomiIndex, mishnaYomiStart,
+  NachYomiEvent, NachYomiIndex, nachYomiStart, 
+  yerushalmiYomi, YerushalmiYomiEvent, vilna, schottenstein
+};
