d78693d5d0d27834e516131e894bb71f800b4e0c
Michael J. Radwin <michael@radwin.org>
2025-08-02 20:55:52 -0700

    Add startDate for seferHaMitzvot

--- src/seferHaMitzvot.ts
+++ src/seferHaMitzvot.ts
@@ -1,11 +1,16 @@
 import {HDate, DailyLearning} from '@hebcal/core';
-import {seferHaMitzvot, seferHaMitzvotStart} from './seferHaMitzvotBase';
+import {
+  seferHaMitzvot,
+  seferHaMitzvotStart as startAbs,
+} from './seferHaMitzvotBase';
 import {SeferHaMitzvotEvent} from './SeferHaMitzvotEvent';
 
-DailyLearning.addCalendar('seferHaMitzvot', (hd: HDate) => {
-  if (hd.abs() < seferHaMitzvotStart) {
+function wrapper(hd: HDate) {
+  if (hd.abs() < startAbs) {
     return null;
   }
   const reading = seferHaMitzvot(hd);
   return new SeferHaMitzvotEvent(hd, reading);
-});
+}
+
+DailyLearning.addCalendar('seferHaMitzvot', wrapper, new HDate(startAbs));
