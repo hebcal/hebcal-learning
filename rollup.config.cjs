e2da8f6f10af87eae32159da40993672c5150136
Michael J. Radwin <michael@radwin.org>
2026-04-10 06:54:26 -0700

    Update typescript, drop iife bundle.js

--- rollup.config.cjs
+++ rollup.config.cjs
@@ -1,18 +1,8 @@
 const json = require('@rollup/plugin-json');
-const terser = require('@rollup/plugin-terser');
 const typescript = require('@rollup/plugin-typescript');
 const pkg = require('./package.json');
 
 const banner = '/*! ' + pkg.name + ' v' + pkg.version + ' */';
-
-const iifeGlobals = {
-  '@hebcal/hdate': 'hebcal',
-  '@hebcal/core': 'hebcal',
-  '@hebcal/core/dist/esm/locale': 'hebcal',
-  '@hebcal/core/dist/esm/event': 'hebcal',
-  '@hebcal/core/dist/esm/holidays': 'hebcal',
-  '@hebcal/core/dist/esm/DailyLearning': 'hebcal',
-};
 
 module.exports = [
   {
@@ -31,29 +21,6 @@ module.exports = [
       json({compact: true, preferConst: true}),
       typescript({outDir: 'dist/esm'}),
     ],
-    external: [/@hebcal/],
-  },
-  {
-    input: 'src/index.ts',
-    output: [
-      {
-        file: 'dist/bundle.js',
-        format: 'iife',
-        name: 'hebcal__learning',
-        globals: iifeGlobals,
-        indent: false,
-        banner,
-      },
-      {
-        file: 'dist/bundle.min.js',
-        format: 'iife',
-        name: 'hebcal__learning',
-        globals: iifeGlobals,
-        plugins: [terser()],
-        banner,
-      },
-    ],
-    plugins: [json({compact: true, preferConst: true}), typescript()],
     external: [/@hebcal/],
   },
 ];
